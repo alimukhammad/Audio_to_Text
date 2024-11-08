f99b55e9495615426be6113557b7f200f5ed17b8
alimukhammad <47307714+alimukhammad@users.noreply.github.com>
2024-11-07 23:34:08 -0500

    Update audio_to_text.py

--- audio_to_text.py
+++ audio_to_text.py
@@ -1,14 +1,14 @@
 
-import speech_recognition as sr
+import speech_recognition as sr # importing required library
 
-r = sr.Recognizer()
+r = sr.Recognizer() #creating a class to recognize the speech
 
-with sr.AudioFile('audio_files_harvard.wav') as source:
-    audio_text = r.listen(source)
+with sr.AudioFile('audio_files_harvard.wav') as source: #open the audio_files_harvard.wav -> assign it to sourse
+    audio_text = r.listen(source) # listen to the audio file and store the audio data in the audio_text variable
 
     try:
-        text = r.recognize_google(audio_text)
-        print('Converting audio transcripts into text ')
-        print('--' + text)
+        text = r.recognize_google(audio_text) #google speech recognition service to convert to the audio data then assign to text
+        print('Converting audio transcripts into text ') # print
+        print('--' + text) #print
     except:
-        print('Pardon... Can you repeat')
+        print('Pardon... Can you repeat') # if try fails -> print
